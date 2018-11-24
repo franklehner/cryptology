eeaecc98ba4cb283555a3caa803f484213225167
Frank Lehner <frank@debian.frank>
2018-11-24 18:34:19 +0100

    prime_factors: Add test for prime_factors

---
+++ test/test_prime_factors.py
@@ -0,0 +1,26 @@
+import pytest as _pytest
+
+import lib.prime_factors as _pf
+
+
+@_pytest.mark.parametrize(
+    "num,res", [
+        (0, []),
+        (2, [2]),
+        (3, [3]),
+        (4, [2, 2]),
+        (6, [2, 3]),
+        (8, [2, 2, 2]),
+        (9, [3, 3]),
+        (5*17*31, [5, 17, 31]),
+    ]
+)
+def test_prime_factor(num, res):
+    assert _pf.prime_factor(num) == res
+
+
+@_pytest.mark.parametrize(
+    "num", ["", 2.1]
+)
+def test_prime_factors_invalid(num):
+    assert _pytest.raises(TypeError, "_pf.prime_factor({0})".format(num))
