f7c81c823af3596f31591562b80557ea308ec7b9
Frank Lehner <frank@debian.frank>
2018-12-01 19:01:44 +0100

    encrypt_monoalphabetical_chiffre: Add filename to the client

--- scripts/encrypt_monoalphabetical_chiffre.py
+++ scripts/encrypt_monoalphabetical_chiffre.py
@@ -12,19 +12,22 @@ The user gives a password and a char as Parameter from the commandline
 from __future__ import print_function
 
 import click as _click
+import lib.app.encrypt_monoalphabetical_cipher
 
 
 @_click.command()
 @_click.option("--password", "-p", required=True, help="Specify the password")
 @_click.option("--sign", "-s", required=True, help="Specify the sign")
-def cli(password, sign):
+@_click.option("--filename", "-f", required=True, help="Specify file with text")
+def cli(password, sign, filename):
     """
     Encrypt monoaphetically
     Params:
         - password str
         - sign char
+        - file str
     """
-    print(password, sign)
+    print(password, sign, filename)
 
 
 if __name__ == "__main__":
