20811b2150df1c4763ea532bf92f613a51df9341
Frank Lehner <frank.lehner@online.de>
2018-12-01 16:07:49 +0100

    encrypt_monoalphabetical_chiffre: Initial commit

---
+++ lib/domain/entities/encrypt.py
@@ -0,0 +1,22 @@
+#pylint: disable=too-few-public-methods
+"""
+encrypt.py
+==========
+"""
+
+
+import abc as _abc
+
+
+class AbstractEncryption(object):
+    """
+    Abstract class for Encryption
+    """
+    __metaclass__ = _abc.ABCMeta
+
+    @_abc.abstractmethod
+    def encrypt(self):
+        """
+        Abstract method encrypt
+        """
+        pass
