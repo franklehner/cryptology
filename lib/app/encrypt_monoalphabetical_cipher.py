adf52370d1f22bf51aed7159347f9b31de4c9300
Frank Lehner <frank@debian.frank>
2018-12-01 18:59:36 +0100

    encrypt_monoalphabetical_cipher: Add Filehandler

---
+++ lib/app/encrypt_monoalphabetical_cipher.py
@@ -0,0 +1,32 @@
+"""
+encrypt_monoalphabetical_cipher.py
+==================================
+
+This script is the real client
+"""
+
+
+import os as _os
+
+
+class FileHandler(object):
+    """
+    Findout the filetype and open it
+    """
+    def __init__(self, filename):
+        """
+        Constructor
+        """
+        self.filename = filename
+
+    def specify_filetype(self):
+        """
+        Find out which filetype is used
+        """
+        return
+
+    def get_text(self):
+        """
+        Return the text from file
+        """
+        return
