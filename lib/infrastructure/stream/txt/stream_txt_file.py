ef46cb261ba1625a2f2588b0be15bb7f75b5917d
Frank Lehner <frank@debian.frank>
2018-12-22 15:38:36 +0100

    file_reader: Initial commit

---
+++ lib/infrastructure/stream/txt/stream_txt_file.py
@@ -0,0 +1,42 @@
+"""
+stream_txt_file.py
+==================
+
+read and write textfile
+"""
+
+
+import os as _os
+import attr as _attr
+
+import lib.domain.entities.file_reader as _file_reader
+
+
+@_attr.s
+class TextFileReader(_file_reader.AbstractReader):
+    """
+    Open text files
+    """
+    filename = _attr.ib()
+    @filename.validator
+    def validate_filename(self, attribute, value): #pylint: disable=unused-argument
+        """
+        validate filename
+        """
+        if not value.endswith(".txt"):
+            raise TypeError("Please use txt file")
+
+        if not _os.path.exists(self.filename):
+            raise IOError("File does not exist")
+
+    def read(self):
+        """
+        read text file
+        """
+        return
+
+    def write(self):
+        """
+        write text file
+        """
+        pass
---
+++ test/test_txt_file_reader.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Test for text file reader
+"""
+
+
+import pytest as _pytest
+
+import lib.infrastructure.stream.txt.stream_txt_file as _txt_file
+
+
+def test_constructor_valid_filename():
+    """
+    Test the constructor
+    """
+    filename = "data/sample_text.txt"
+    text_reader = _txt_file.TextFileReader(filename)
+    assert filename == text_reader.filename
+
+
+def test_wrong_file_type():
+    """
+    Test the wrong file type
+    """
+    filename = "data/sample.pdf"
+    assert _pytest.raises(
+        TypeError,
+        "_txt_file.TextFileReader(filename='{0}')".format(filename)
+    )
+
+
+def test_file_does_not_exist():
+    """
+    Test invalid filename
+    """
+    filename = "foo.txt"
+    assert _pytest.raises(IOError, "_txt_file.TextFileReader(filename='{0}')".format(filename))
