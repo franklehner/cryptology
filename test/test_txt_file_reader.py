8a9601c5c7ab8b5b019100a8144794fc43e2024e
Frank Lehner <frank.lehner@online.de>
2018-12-24 11:52:41 +0100

    test_txt_file_reader: Add test for file reader

--- test/test_txt_file_reader.py
+++ test/test_txt_file_reader.py
@@ -18,11 +18,16 @@ def test_constructor_valid_filename():
     assert filename == text_reader.filename
 
 
-def test_wrong_file_type():
+@_pytest.mark.parametrize(
+    "filename", [
+        "data/1026778984.pdf",
+        "data/1026778984.gz"
+    ]
+)
+def test_wrong_file_type(filename):
     """
     Test the wrong file type
     """
-    filename = "data/sample.pdf"
     assert _pytest.raises(
         TypeError,
         "_txt_file.TextFileReader(filename='{0}')".format(filename)
@@ -38,3 +43,15 @@ def test_file_does_not_exist():
         IOError,
         "_txt_file.TextFileReader(filename='{0}')".format(filename)
     )
+
+
+def test_read_text():
+    """
+    test read text
+    """
+    filename = "data/1026778984.txt"
+    text_reader = _txt_file.TextFileReader(filename=filename)
+    text = text_reader.read()
+    assert text
+    assert isinstance(text, (str, unicode))
+    assert len(text) == 393566
