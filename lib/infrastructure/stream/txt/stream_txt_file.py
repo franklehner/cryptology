6b36a204168d18d6717f59c50327b046809bea53
Frank Lehner <frank.lehner@online.de>
2019-01-04 23:33:44 +0100

    Refactoring

--- lib/infrastructure/stream/txt/stream_txt_file.py
+++ lib/infrastructure/stream/txt/stream_txt_file.py
@@ -9,11 +9,11 @@ read and write textfile
 import os as _os
 import attr as _attr
 
-import lib.domain.entities.file_reader as _file_reader
+import lib.domain.entities.file_stream as _file_stream
 
 
 @_attr.s
-class TextFile(_file_reader.AbstractFileIO):
+class TextFile(_file_stream.AbstractFileIO):
     """
     Open text files
     """
@@ -23,22 +23,27 @@ class TextFile(_file_reader.AbstractFileIO):
         """
         validate filename
         """
+        if value == self.filename:
+            pass
+
         if not value.endswith(".txt"):
             raise TypeError("Please use txt file")
-
-        if not _os.path.exists(self.filename):
-            raise IOError("File does not exist")
 
     def read(self):
         """
         read text file
         """
+        if not _os.path.exists(self.filename):
+            raise IOError("File does not exist")
+
         with open(self.filename) as text_file:
             text = text_file.read()
         return text
 
-    def write(self):
+    def write(self, text):
         """
         write text file
         """
-        pass
+        with open(self.filename, "w") as file_stream:
+            for line in text:
+                file_stream.write(line)
--- test/test_txt_file_stream.py
+++ test/test_txt_file_stream.py
@@ -4,9 +4,22 @@ Test for text file reader
 """
 
 
+import os as _os
 import pytest as _pytest
 
 import lib.infrastructure.stream.txt.stream_txt_file as _txt_file
+
+
+def create_text(multiline=False):
+    """
+    create a sample text
+    """
+    if not multiline:
+        text = "Hallo Entschen"
+    else:
+        text = "Hallo Entschen\nwie geht es dir"
+
+    return text
 
 
 def test_constructor_valid_filename():
@@ -14,8 +27,8 @@ def test_constructor_valid_filename():
     Test the constructor
     """
     filename = "data/1026778984.txt"
-    text_reader = _txt_file.TextFile(filename)
-    assert filename == text_reader.filename
+    text_file = _txt_file.TextFile(filename)
+    assert filename == text_file.filename
 
 
 @_pytest.mark.parametrize(
@@ -41,7 +54,7 @@ def test_file_does_not_exist():
     filename = "foo.txt"
     assert _pytest.raises(
         IOError,
-        "_txt_file.TextFile(filename='{0}')".format(filename)
+        "_txt_file.TextFile(filename='{0}').read()".format(filename)
     )
 
 
@@ -50,8 +63,28 @@ def test_read_text():
     test read text
     """
     filename = "data/1026778984.txt"
-    text_reader = _txt_file.TextFile(filename=filename)
-    text = text_reader.read()
+    text_file = _txt_file.TextFile(filename=filename)
+    text = text_file.read()
     assert text
     assert isinstance(text, (str, unicode))
     assert len(text) == 393566
+
+@_pytest.mark.parametrize(
+    "multiline", [
+        False,
+        True
+    ]
+)
+def test_write_file(multiline):
+    """
+    test writing file
+    """
+    filename = "/tmp/foo.txt"
+    text = create_text(multiline)
+    text_file = _txt_file.TextFile(filename)
+    text_file.write(text)
+    assert _os.path.exists(filename)
+    with open(filename) as file_stream:
+        text_stream = file_stream.read().strip()
+
+    assert text_stream == text
