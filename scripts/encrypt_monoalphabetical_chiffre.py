7f599a26ef085ca0d638c2543f0e220fe08665b3
Frank Lehner <frank.lehner@online.de>
2019-01-09 22:20:03 +0100

    scripts/encrypt_monoalphabetical_chiffre: Finish

--- scripts/encrypt_monoalphabetical_chiffre.py
+++ scripts/encrypt_monoalphabetical_chiffre.py
@@ -12,14 +12,15 @@ The user gives a password and a char as Parameter from the commandline
 from __future__ import print_function
 
 import click as _click
-import lib.app.encrypt_monoalphabetical_cipher
+import lib.app.encrypt_monoalphabetical_cipher as _encrypter
 
 
 @_click.command()
 @_click.option("--password", "-p", required=True, help="Specify the password")
 @_click.option("--sign", "-s", required=True, help="Specify the sign")
 @_click.option("--filename", "-f", required=True, help="Specify file with text")
-def cli(password, sign, filename):
+@_click.option("--out_file", "-o", required=True, help="Specify out file")
+def cli(password, sign, filename, out_file):
     """
     Encrypt monoaphetically
     Params:
@@ -27,7 +28,10 @@ def cli(password, sign, filename):
         - sign char
         - file str
     """
-    print(password, sign, filename)
+    encrypter = _encrypter.Encrypter(filename)
+    text = encrypter.get_text()
+    encrypted_text = encrypter.encrypt(password, sign, text)
+    encrypter.write(out_file, encrypted_text)
 
 
 if __name__ == "__main__":
